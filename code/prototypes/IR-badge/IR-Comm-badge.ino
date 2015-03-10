24a506281dc8b64a354fa689781226e1722e7966
Mike Emerick <mkemeric@gmail.com>
2015-03-09 23:56:56 -0500

    Use IR-Comm-Badge.ino significant updates now intagrated image with ir recieve and transmit. Using the same parts as production badge

--- code/prototypes/IR-badge/IR-Comm-badge.ino
+++ code/prototypes/IR-badge/IR-Comm-badge.ino
@@ -26,7 +26,9 @@
 #define IRpin_PIN      PIND
 #define IRpin          4
 //#define erasePin       2
-//#define ledPin         6
+#define ledPin         6
+#define IRledPin1      13
+#define IRledPin2      12
 //#define soundPin       12
 #define DEBUG
 //#define NUMPIXELS 7
@@ -35,13 +37,13 @@
 
 
 // the maximum pulse we'll listen for - 65 milliseconds is a long time
-#define MAXPULSE 65000
+#define MAXPULSE 60000
 #define NUMPULSES 50
 
 // what our timing resolution should be, larger is better
 // as its more 'precise' - but too large and you wont get
 // accurate timing
-#define RESOLUTION 20 
+#define RESOLUTION 10 
 //#define RESOLUTION 40 
 
 // What percent we will allow in variation to match the same code
@@ -55,47 +57,63 @@ int Signal[NUMPULSES];  // array where initial signal is stored for compare to s
 void setup(void) {
   //clock_prescale_set(clock_div_1);
   Serial.begin(9600);
-  Serial.println("Ready to decode IR!");
+  Serial.println("Ready to for IR-Comm");
   //Serial.println(CLKPR);
+  pinMode(ledPin, OUTPUT);
+  pinMode(IRledPin1, OUTPUT);
+  pinMode(IRledPin2, OUTPUT);
+  digitalWrite(IRledPin1,LOW);
+  digitalWrite(IRledPin2,LOW);
 }
+
+//void loop (void){
+//  Serial.println("Sending code");
+//  SendNikonCode();
+//  delay (15 * 1000);
+//}
 
 void loop(void) {
   int numberpulses;
-  //UP
-  int Signal_1 [20] = {236,58,58,58,58,58,58,58,60,56,118,58,58,58,58,58,116,58};  //,58,58,58,58,60,56,60,2596,236,58,60,56,60,56,60,58,58,58,116,58,58,58,58,58,116,58,60,58,58,58,58,58,60,2596,236,58,58,58,58,58,58,58,58,58,116,58,60,56,58,60,116,58,58,58,58,58,60,56,58,2598,236,58,58,58,58,58,60,56,60,56,118,56,60,58,58,58,118,56,58,58,60,200};  
-  //Down
-  int Signal_2 [20] = {236,54,120,54,62,56,60,56,60,56,120,54,62,54,62,56,118,56};  //,60,56,62,54,62,54,62,2538,236,56,118,56,60,56,62,54,62,54,120,56,60,56,60,56,120,54,62,54,62,56,60,56,60,2540,236,54,120,56,60,56,60,56,60,56,120,54,62,54,62,54,120,56,62,54,60,56,60,56,60,2540,236,56,118,56,62,54,62,54,62,54,120,56,60,56,60,56,120,54,62,54,62,200};  
-  //reserved for our generated codes
-  int Signal_3 [20] = {236,56,120,54,120,56,118,56,60,56,62,54,60,56,62,54,120,56};  //,60,54,62,56,60,56,60,2482,236,56,118,56,120,54,60,56,62,54,120,56,60,56,60,56,118,56,62,54,62,54,62,54,62,2482,234,56,120,54,120,54,62,56,60,56,118,56,62,54,60,56,120,56,60,56,60,56,60,56,60,2482,236,56,118,56,118,56,62,54,62,54,120,54,62,56,60,56,120,54,62,54,62,200};
-  // reserved for our generated codes
-  int Signal_4 [20] = {236,54,62,54,62,56,60,56,60,56,62,54,62,54,62,54,120,56};  //,62,54,62,56,60,56,60,2540,236,56,60,56,118,56,60,56,62,54,120,54,62,54,62,54,120,56,62,54,60,56,62,54,62,2538,236,56,60,56,120,54,62,54,62,56,118,56,60,56,60,56,120,54,62,54,62,54,62,56,60,2540,236,54,62,54,120,56,60,56,60,56,120,54,62,54,62,56,118,56,60,56,62,200};
-  Serial.println("Please send your signal");
-  //numberpulses = listenForIR();   // we don't actually use the return value here.  
+//  //First_signal
+   int Signal_1 [14] = {226,2660,50,146,54,336,56,6304,226,2620,50,146,52,336};  
+   int Signal_2 [14] = {440,1422,23,285,75,95,29,6153,442,1420,23,280,66,100}; 
+//  int Signal_3 [20] = {236,56,120,54,120,56,118,56,60,56,62,54,60,56,62,54,120,56};  
+//  int Signal_4 [20] = {236,54,62,54,62,56,60,56,60,56,62,54,62,54,62,54,120,56};  
+  Serial.println("Sending Signal 1");
+  Send_Signal_1();
+  Serial.println("Sending Signal 2");
+  Send_Signal_2();
+  Serial.println("Listening for IR");
+  numberpulses = listenForIR();   // we don't actually use the return value here.  
+  //printpulses();
   //convert_to_array();
   //Serial.println("Got one signal I need one more to match against:");
   //delay(500);
-  numberpulses = listenForIR();
+  //numberpulses = listenForIR();
   //Serial.println("I got a second signal let me see if they match");
   if(IRcompare(numberpulses, Signal_1,25)){
       Serial.println("I heard Signal 1");
+      blinkPattern(ledPin,3,500);
   }
   else if(IRcompare(numberpulses, Signal_2,25)){
       Serial.println("I heard Signal 2");
+      blinkPattern(ledPin,6,300);
   }
-  else if(IRcompare(numberpulses, Signal_3,25)){
-      Serial.println("I heard Signal 2");
-  }
-  else if(IRcompare(numberpulses, Signal_4,25)){
-      Serial.println("I heard Signal 4");
-  }
-  else {
-      Serial.println("No signal matched");
-  }  
+//  else if(IRcompare(numberpulses, Signal_3,25)){
+//      Serial.println("I heard Signal 2");
+//  }
+//  else if(IRcompare(numberpulses, Signal_4,25)){
+//      Serial.println("I heard Signal 4");
+//  }
+//  else {
+//      Serial.println("No signal matched");
+//  }  
 }
  
 //KGO: added size of compare sample. Only compare the minimum of the two
 boolean IRcompare(int numpulses, int MY_Signal[], int refsize) {
-  int count = min(10,refsize);
+  //int count = min(10,refsize);
+  int count = 7;
   #ifdef DEBUG
     Serial.print("count set to: ");
     Serial.println(count);
@@ -153,12 +171,10 @@ boolean IRcompare(int numpulses, int MY_Signal[], int refsize) {
 
 int listenForIR(void) {
   currentpulse = 0;
-  
   while (1) {
     uint16_t highpulse, lowpulse;  // temporary storage timing
     highpulse = lowpulse = 0; // start out with no pulse length
   
-//  while (digitalRead(IRpin)) { // this is too slow!
     while (IRpin_PIN & (1 << IRpin)) {
        // pin is still HIGH
 
@@ -222,16 +238,16 @@ void printpulses(void) {
 void convert_to_array (void) {
   // print it in a 'array' format
   //Serial.println("int IRsignal[] = {");
-  //Serial.println("// ON, OFF (in 10's of microseconds)");
+  Serial.println("// ON, OFF (in 10's of microseconds)");
   for (uint8_t i = 0; i < currentpulse-1; i++) {
     //Serial.print("\t"); // tab
     Signal[i*2 + 0] = pulses[i][1] * RESOLUTION / 10;
-    //Serial.print(Signal[i*2 + 0]);
-    //Serial.print(", ");
+    Serial.print(Signal[i*2 + 0]);
+    Serial.print("  ");
     Signal[i*2 + 1] = pulses[i+1][0] * RESOLUTION / 10;
-    //Serial.println(Signal[i*2 + 1]);
+    Serial.println(Signal[i*2 + 1]);
   }
-  //Serial.println("\n");
+  Serial.println("\n");
 }
 
 void print_signal (void) {
@@ -261,18 +277,96 @@ void print_signal (void) {
 //      }
 //}
 //
-//void blinkPattern(int tempPin, int numBlinks, int tempDuration){
-//  int delaytime = 100;
-// // = tempDuration -360; 
-//  for (int i = 1; i <= numBlinks;i++){ 
-//    digitalWrite(tempPin, HIGH);
-//    //hitSFX();
-//    delay(delaytime);
-//    digitalWrite(tempPin, LOW);
-//    //hitSFX();
-//    delay(delaytime);
-//  }
-//}
+
+void blinkPattern(int tempPin, int numBlinks, int tempDuration){
+  for (int i = 1; i <= numBlinks;i++){ 
+    digitalWrite(tempPin, HIGH);
+    //hitSFX();
+    delay(tempDuration);
+    digitalWrite(tempPin, LOW);
+    //hitSFX();
+    delay(tempDuration);
+  }
+}
+
+// This procedure sends a 38KHz pulse to the IRledPin 
+// for a certain # of microseconds. We'll use this whenever we need to send codes
+void pulseIR(long microsecs) {
+  // we'll count down from the number of microseconds we are told to wait
+ 
+  cli();  // this turns off any background interrupts
+ 
+  while (microsecs > 0) {
+    // 38 kHz is about 13 microseconds high and 13 microseconds low
+   //digitalWrite(IRledPin1, LOW);  // this takes about 3 microseconds to happen
+   digitalWrite(IRledPin2, HIGH);
+   delayMicroseconds(10);         // hang out for 10 microseconds, you can also change this to 9 if its not working
+   //digitalWrite(IRledPin1, HIGH);   // this also takes about 3 microseconds
+   digitalWrite(IRledPin2, LOW);
+   delayMicroseconds(10);         // hang out for 10 microseconds, you can also change this to 9 if its not working
+ 
+   // so 26 microseconds altogether
+   microsecs -= 26;
+  }
+ 
+  sei();  // this turns them back on
+}
+ 
+void Send_Signal_1() {
+  // This is the code for my particular Nikon, for others use the tutorial
+  // to 'grab' the proper code from the remote
+ 
+  pulseIR(2000);
+  delay(27);
+  pulseIR(440);
+  delayMicroseconds(1500);
+  pulseIR(460);
+  delayMicroseconds(3440);
+  pulseIR(480);
+  //digitalWrite(IRledPin1,LOW);
+  //digitalWrite(IRledPin2,LOW);
+ 
+ 
+  delay(65); // wait 65 milliseconds before sending it again
+ 
+  pulseIR(2000);
+  delay(27);
+  pulseIR(440);
+  delayMicroseconds(1500);
+  pulseIR(460);
+  delayMicroseconds(3440);
+  pulseIR(480);
+  //digitalWrite(IRledPin1,LOW);
+  //digitalWrite(IRledPin2,LOW);
+  
+}
+
+void Send_Signal_2() {
+  // This is the code for my particular Nikon, for others use the tutorial
+  // to 'grab' the proper code from the remote
+ 
+  pulseIR(4000);
+  delay(15);
+  pulseIR(200);
+  delayMicroseconds(3000);
+  pulseIR(660);
+  delayMicroseconds(1000);
+  pulseIR(260);
+
+ 
+ 
+  delay(65); // wait 65 milliseconds before sending it again
+ 
+  pulseIR(4000);
+  delay(15);
+  pulseIR(200);
+  delayMicroseconds(3000);
+  pulseIR(660);
+  delayMicroseconds(1000);
+  pulseIR(260);
+
+  
+}
 
 //void neo_LED (int tempDuration){
 //  for(int i=0;i<NUMPIXELS;i++){
