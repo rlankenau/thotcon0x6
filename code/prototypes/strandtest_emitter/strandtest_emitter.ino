6f689fc5de0c654f539b22ea9bc817bc44563df4
Rudy <rudy.ristich@gmail.com>
2015-05-05 20:52:00 -0500

    Revert "boot sequence"
    This reverts commit 990b869b8c648723e8cab18e1544e45a2ad0623f.

--- code/prototypes/strandtest_emitter/strandtest_emitter.ino
+++ code/prototypes/strandtest_emitter/strandtest_emitter.ino
@@ -13,7 +13,7 @@
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(7, STRAND, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(6, STRAND, NEO_GRB + NEO_KHZ800);
 int serial_init = 0;
 
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
